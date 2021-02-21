b30225a60592ee54e4741075b216459623527a45
mvamsi73 <73768311+mvamsi73@users.noreply.github.com>
2021-02-21 10:52:47 +0530

    servlets_CRUD_Operations

--- servletsCRUD/src/update2.java
+++ servletsCRUD/src/update2.java
@@ -23,10 +23,6 @@ public class update2 extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
